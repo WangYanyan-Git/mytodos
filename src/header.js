54525d1d1036b75a2cc828fe3e7de225daf72cb4
wyy <137963051@qq.com>
2019-10-03 20:19:40 +0800

    删除多余文件

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Headers extends React.Component{
+
+   render(){
+      return (
+          <header>
+             top
+          </header>
+      )
+   }
+
+}
+
+export default Headers;
