5a29e317fda3fcaa234b426e573ab4fafece8332
wangyanyan998 <137963051@qq.com>
2019-11-03 12:57:05 +0800

    add 2.js

---
+++ src/2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './header';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+    </div>
+  );
+}
+
+export default App;
